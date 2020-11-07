7b27b4ae10aa1368f45f5bd93941761a058c84f8
Odatha <bandaraods@gmail.com>
2020-11-07 17:28:54 +0530

    Used node aynchronous and callback implementation

--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,30 @@
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => (x, y) => x * y;
+//exports.perimeter = (x, y) => 2 * (x + y);
+//exports.area = (x, y) => x * y;
 
-var rect = require("./rectangle");
+module.exports = (x, y, callback) => {
+  //use of first class function -callback
+  if (x <= 0 || y <= 0)
+    setTimeout(
+      //takes two parameters, callback and the time
+      () =>
+        callback(
+          new Error(
+            "Rectangle dimensions should be greater than zero: l = " +
+              x +
+              ", and b = " +
+              y
+          ),
+          null
+        ),
+      2000
+    );
+  else
+    setTimeout(
+      () =>
+        callback(null, {
+          perimeter: () => 2 * (x + y),
+          area: () => x * y, // return value is sent back to the index module
+        }),
+      2000
+    );
+};
